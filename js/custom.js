522a6f196f547233275c1d8a41f5afe2f416ca59
Ananya <ananyagoyal128@gmail.com>
2023-01-31 11:44:09 +0530

    small changes done

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+const actualBtn = document.getElementById('getFile');
+
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.innerText = this.files[0].name
+})
